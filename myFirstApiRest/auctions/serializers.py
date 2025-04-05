fbc378465089704aa1a5cd9a97e95f508fc8d2ae
Rodrigo Covas Vasquez <202210672@alu.comillas.edu>
2025-04-05 12:50:00 +0200

    Django Server y Tutoriales APIREST

---
+++ myFirstApiRest/auctions/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+from .models import Category, Auction, Bid
+from django.utils import timezone
+from drf_spectacular.utils import extend_schema_field
+
+class CategoryListCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','name']
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class AuctionListCreateSerializer(serializers.ModelSerializer):
+    creation_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ",read_only=True)
+    closing_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ")
+    isOpen = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Auction
+        fields = '__all__'
+
+    @extend_schema_field(serializers.BooleanField())
+    def get_isOpen(self, obj):
+        return obj.closing_date > timezone.now()
+    
+    def validate_closing_date(self, value):
+        if value <= timezone.now():
+            raise serializers.ValidationError("Closing date must be greater than now.")
+        return value
+
+
+class AuctionDetailSerializer(serializers.ModelSerializer):
+    creation_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ", read_only=True)
+    closing_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ")
+    isOpen = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Auction
+        fields = '__all__'
+
+    @extend_schema_field(serializers.BooleanField())
+    def get_isOpen(self, obj):
+        return obj.closing_date > timezone.now()
+    
+    def validate_closing_date(self, value):
+        if value <= timezone.now():
+            raise serializers.ValidationError("Closing date must be greater than now.")
+        return value
+    
+class BidListCreateSerializer(serializers.ModelSerializer):
+    creation_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ", read_only=True)
+    
+    class Meta:
+        model = Bid
+        fields = ['id', 'auction', 'price', 'creation_date', 'bidder']
+
+class BidDetailSerializer(serializers.ModelSerializer):
+    creation_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ", read_only=True)
+    auction_title = serializers.ReadOnlyField(source='auction.title')
+
+    class Meta:
+        model = Bid
+        fields = ['id', 'auction', 'auction_title', 'price', 'creation_date', 'bidder']
---
+++ myFirstApiRest/auctions/views.py
@@ -0,0 +1,51 @@
+from rest_framework import generics
+from .models import Category, Auction, Bid
+from .serializers import (
+    CategoryListCreateSerializer,
+    CategoryDetailSerializer,
+    AuctionListCreateSerializer,
+    AuctionDetailSerializer,
+    BidListCreateSerializer,
+    BidDetailSerializer,
+)
+
+
+class CategoryListCreate(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryListCreateSerializer
+
+
+class CategoryRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryDetailSerializer
+
+
+class AuctionListCreate(generics.ListCreateAPIView):
+    queryset = Auction.objects.all()
+    serializer_class = AuctionListCreateSerializer
+
+
+class AuctionRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Auction.objects.all()
+    serializer_class = AuctionDetailSerializer
+
+
+class BidListCreate(generics.ListCreateAPIView):
+    serializer_class = BidListCreateSerializer
+
+    def get_queryset(self):
+        auction_id = self.kwargs["auction_id"]
+        return Bid.objects.filter(auction_id=auction_id)
+
+    def perform_create(self, serializer):
+        auction_id = self.kwargs["auction_id"]
+        serializer.save(auction_id=auction_id)
+
+
+class BidRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bid.objects.all()
+    serializer_class = BidDetailSerializer
+
+    def get_queryset(self):
+        auction_id = self.kwargs["auction_id"]
+        return super().get_queryset().filter(auction_id=auction_id)
